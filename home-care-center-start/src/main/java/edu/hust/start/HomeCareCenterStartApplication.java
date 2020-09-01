376c92829ea82c9140505e145a5758f950a61529
Derry Lin <1757548143@qq.com>
2020-09-01 15:07:58 +0800

    Spring Boot Init

---
+++ home-care-center-start/src/main/java/edu/hust/start/HomeCareCenterStartApplication.java
@@ -0,0 +1,13 @@
+package edu.hust.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeCareCenterStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeCareCenterStartApplication.class, args);
+    }
+
+}
---
+++ home-care-center-start/src/test/java/edu/hust/start/HomeCareCenterStartApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.hust.start;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeCareCenterStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
