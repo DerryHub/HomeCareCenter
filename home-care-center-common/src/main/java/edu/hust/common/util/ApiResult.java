dc1456d19fc9704b9b672b60f652d296faadc8ac
chenchain <1759683080@qq.com>
2020-09-02 11:30:44 +0800

    修改json返回

--- home-care-center-common/src/main/java/edu/hust/common/util/ApiResult.java
+++ home-care-center-common/src/main/java/edu/hust/common/util/ApiResult.java
@@ -1,6 +1,6 @@
-package utils;
+package edu.hust.common.util;
 
-public class JsonData {
+public class ApiResult {
     private int code;
     private Object data;
     private String msg;
@@ -29,30 +29,30 @@ public class JsonData {
         this.msg = msg;
     }
 
-    public JsonData() {
+    public ApiResult() {
     }
 
-    public JsonData(int code, Object data) {
+    public ApiResult(int code, Object data) {
         this.code = code;
         this.data = data;
     }
 
-    public JsonData(int code, Object data, String msg) {
+    public ApiResult(int code, Object data, String msg) {
         this.code = code;
         this.data = data;
         this.msg = msg;
     }
 
-    public static JsonData buildSuccess(Object data){
-        return new JsonData(0, data);
+    public static ApiResult buildSuccess(Object data){
+        return new ApiResult(0, data);
     }
 
-    public static JsonData buildError(String msg){
-        return new JsonData(-1, "", msg);
+    public static ApiResult buildError(String msg){
+        return new ApiResult(-1, "", msg);
     }
 
-    public static JsonData buildError(String msg, int code){
-        return new JsonData(code, "", msg);
+    public static ApiResult buildError(String msg, int code){
+        return new ApiResult(code, "", msg);
     }
 
     @Override
