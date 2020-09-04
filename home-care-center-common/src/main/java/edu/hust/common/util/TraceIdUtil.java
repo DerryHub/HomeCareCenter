52707d56affe0f0f9cf1f002c34700e1eec7cf8e
chenchain <1759683080@qq.com>
2020-09-04 10:57:20 +0800

    日志拦截器

--- home-care-center-common/src/main/java/edu/hust/common/util/TraceIdUtil.java
+++ home-care-center-common/src/main/java/edu/hust/common/util/TraceIdUtil.java
@@ -1,9 +1,14 @@
 package edu.hust.common.util;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.concurrent.BasicThreadFactory;
+import org.slf4j.Logger;
 import org.slf4j.MDC;
 
+import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.*;
 
 /**
  *
@@ -13,6 +18,7 @@ import java.util.UUID;
  * https://www.cnblogs.com/warking/p/5710303.html
  *
  **/
+@Slf4j
 public class TraceIdUtil {
 
     public static final String TRACE_ID="traceId";
@@ -35,6 +41,12 @@ public class TraceIdUtil {
         MDC.put(TRACE_ID,traceId);
     }
 
+    public static void  setTraceIdIfAbsent(){
+        if (MDC.get(TRACE_ID)==null){
+            MDC.put(TRACE_ID,getOneTraceId());
+        }
+    }
+
     /**
      * 在当前上下文中设置指定的traceId
      */
@@ -54,6 +66,101 @@ public class TraceIdUtil {
      */
     public static void clearTraceId(){
         MDC.remove(TRACE_ID);
+    }
+
+
+    public ThreadPoolExecutor createThreadPoolExecutor(){
+        BasicThreadFactory threadFactory = new BasicThreadFactory.Builder().namingPattern("query-pool-%d")
+                .uncaughtExceptionHandler(new LogErrorExceptionHandler())
+                .daemon(true).build();
+        //查询使用的线程池，使用50个线程,最大100个线程，线程有效期为360秒，queue大小为200
+        ThreadPoolExecutor threadPoolExecutor;
+        threadPoolExecutor = new ThreadPoolTaskExecutorMdcWrapper(50, 100,
+                360L, TimeUnit.SECONDS,
+                new ArrayBlockingQueue<>(200), threadFactory,
+                (r, executor) -> log.error("common pool list is full, activeCount{}, maxiMumSize{}",
+                        executor.getActiveCount(), executor.getMaximumPoolSize()));
+        return threadPoolExecutor;
+    }
+
+
+    private static <T>Callable<T> wrapper(Callable<T> callable, Map<String,String> context){
+        return ()->{
+            if (context==null){
+                MDC.clear();
+            }else {
+                MDC.setContextMap(context);
+            }
+            setTraceIdIfAbsent();
+            try {
+                return  callable.call();
+            }finally {
+                clearTraceId();
+            }
+        };
+    }
+
+    private static Runnable wrapper(Runnable runnable,Map<String,String> context){
+        return ()->{
+            if (context==null){
+                MDC.clear();
+            }else {
+                MDC.setContextMap(context);
+            }
+            setTraceIdIfAbsent();
+            try {
+                runnable.run();
+            }finally {
+                clearTraceId();
+            }
+        };
+    }
+
+    private static class ThreadPoolTaskExecutorMdcWrapper extends ThreadPoolExecutor{
+
+        public ThreadPoolTaskExecutorMdcWrapper(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue) {
+            super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);
+        }
+
+        public ThreadPoolTaskExecutorMdcWrapper(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue, ThreadFactory threadFactory) {
+            super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, threadFactory);
+        }
+
+        public ThreadPoolTaskExecutorMdcWrapper(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue, RejectedExecutionHandler handler) {
+            super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, handler);
+        }
+
+        public ThreadPoolTaskExecutorMdcWrapper(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue, ThreadFactory threadFactory, RejectedExecutionHandler handler) {
+            super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, threadFactory, handler);
+        }
+        @Override
+        public void execute(Runnable task) {
+            super.execute(TraceIdUtil.wrapper(task, MDC.getCopyOfContextMap()));
+        }
+
+        @Override
+        public <T> Future<T> submit(Runnable task, T result) {
+            return super.submit(TraceIdUtil.wrapper(task, MDC.getCopyOfContextMap()), result);
+        }
+
+        @Override
+        public <T> Future<T> submit(Callable<T> task) {
+            return super.submit(TraceIdUtil.wrapper(task, MDC.getCopyOfContextMap()));
+        }
+
+        @Override
+        public Future<?> submit(Runnable task) {
+            return super.submit(TraceIdUtil.wrapper(task, MDC.getCopyOfContextMap()));
+        }
+    }
+
+
+    private static class LogErrorExceptionHandler implements Thread.UncaughtExceptionHandler{
+
+        @Override
+        public void uncaughtException(Thread t, Throwable e) {
+            log.error(e.getMessage(),t);
+        }
     }
 
 }
--- home-care-center-start/src/main/java/edu/hust/start/HomeCareCenterStartApplication.java
+++ home-care-center-start/src/main/java/edu/hust/start/HomeCareCenterStartApplication.java
@@ -1,24 +1,45 @@
 package edu.hust.start;
 
+import edu.hust.common.util.TraceIdUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.List;
+import java.util.concurrent.ThreadPoolExecutor;
 
+@Slf4j
 @SpringBootApplication
 @MapperScan("edu.hust.dao.dao")
 public class HomeCareCenterStartApplication {
 
 
+
     public static void main(String[] args) {
+        ThreadPoolExecutor threadPoolExecutor=new TraceIdUtil().createThreadPoolExecutor();
+
         SpringApplication.run(HomeCareCenterStartApplication.class, args);
+        MDC.put("traceId","testId");
+
+        threadPoolExecutor.submit(()->{
+            log.info("this is a thread 1");
+        });
+        threadPoolExecutor.submit(()->{
+            log.info("this is a thread 2");
+        });
+
+
+        new Thread(()->{
+            log.info("this is a thread 2");
+        }).start();
+        new Thread(()->{
+            log.info("this is a thread 3");
+        }).start();
 
     }
 
-    public void test(){
-
-    }
 
 }
